fd7b2e43042e3b7214199413e23dd50e2f23db8a
elieserharmonia <eliesermusicoccb@gmail.com>
2026-06-10 14:25:20 -0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "tanamao-cache-v1";
+const OFFLINE_URL = "/offline.html";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([OFFLINE_URL]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(OFFLINE_URL))
+  );
+});
