93f169e1b73c8b90ba9c2283af2a87f7dfff657d
elieserharmonia <eliesermusicoccb@gmail.com>
2026-06-10 14:28:16 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./styles/globals.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+// Registro do Service Worker para PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((reg) => console.log('Service Worker registrado com sucesso:', reg))
+      .catch((err) => console.log('Erro ao registrar Service Worker:', err));
+  });
+}
